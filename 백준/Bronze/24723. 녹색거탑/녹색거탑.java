d2fa975934d8510b043cf10e30b660ebe892f668
peaple14 <92345015+peaple14@users.noreply.github.com>
2024-09-25 11:11:01 +0900

    [Bronze IV] Title: 녹색거탑, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+
+
+        System.out.println((int)Math.pow(2,a));
+    }
+
+}
