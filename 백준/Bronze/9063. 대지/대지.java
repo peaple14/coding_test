5eb248ea9e7ef3ac5f93dde1a8c57a0ad693cac6
peaple14 <92345015+peaple14@users.noreply.github.com>
2024-10-31 09:06:50 +0900

    [Bronze III] Title: 대지, Time: 260 ms, Memory: 27456 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        //자바에서 제공하는 ???_VALUE
+        int min_x = Integer.MAX_VALUE;
+        int min_y = Integer.MAX_VALUE;
+        int max_x = Integer.MIN_VALUE;
+        int max_y = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            min_x = Math.min(min_x, x);
+            min_y = Math.min(min_y, y);
+            max_x = Math.max(max_x, x);
+            max_y = Math.max(max_y, y);
+        }
+
+        int area = (max_x - min_x) * (max_y - min_y);
+        
+        if (n == 1) {
+            System.out.println(0);
+        } else {
+            System.out.println(area);
+        }
+    }
+}
